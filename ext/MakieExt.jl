71bde5b2b2dae1ea579043062e64a3c7f73bcd1c
kaandocal <26488673+kaandocal@users.noreply.github.com>
2025-12-11 09:02:52 +1100

    Improved plotting code somewhat

--- ext/MakieExt.jl
+++ ext/MakieExt.jl
@@ -2,18 +2,21 @@ module MakieExt
 
 using Chemostats, Makie
 
-# Use structarrays
 function Makie.plot(chem::Chemostat; tspan=(0, Chemostats.get_curr_t(chem)), Λ_gt=nothing, alg=nothing)
     fig = Figure()
 
     tt = [ snap.t for snap in chem.saved ]
-    NN = [ snap.N for snap in chem.saved ]
+    NN = [ Chemostats.est_N(snap) for snap in chem.saved ]
 
     ax_N = Axis(fig[1,1], xlabel="Time", ylabel="N", yscale=maximum(NN) > 10 ? log10 : identity)
     ax_Λ = Axis(fig[2,1], xlabel="Time", ylabel="Λ")
-    ax_δ = Axis(fig[3,1], xlabel="Time", ylabel="δ")
+    ax_δ = if alg isa Chemostats.Lax
+        Axis(fig[3,1], xlabel="Time", ylabel="δ")
+    else 
+        nothing 
+    end
 
-    linkxaxes!([ax_N, ax_Λ, ax_δ])
+    linkxaxes!([ax_N, ax_Λ])
     snaps = filter(snap -> tspan[1] <= snap.t <= tspan[2], chem.saved)
 
     lines!(ax_N, tt, NN)
@@ -32,6 +35,7 @@ function Makie.plot(chem::Chemostat; tspan=(0, Chemostats.get_curr_t(chem)), Λ_
     isnothing(Λ_gt) || hlines!(ax_Λ, Λ_gt; linestyle=:dash, color=:black)
 
     if alg isa Chemostats.Lax
+        linkxaxes!([ax_N, ax_δ])
         δδ = [ Chemostats.get_δ(chem, t, alg) for t in tt ]
         lines!(ax_δ, tt, δδ)
         isnothing(Λ_gt) || hlines!(ax_δ, Λ_gt; linestyle=:dash, color=:black)
