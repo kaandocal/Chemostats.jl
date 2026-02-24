cf956d49f0188af3e1d7ebed483567a275023b23
kaandocal <26488673+kaandocal@users.noreply.github.com>
2026-02-24 20:35:33 +1100

    Added alg argument to unit tests

--- test/det.jl
+++ test/det.jl
@@ -6,12 +6,13 @@ include("models/deterministic.jl")
 
 Λ_gt = 1.4
 prob = Deterministic(Λ_gt)
+alg = Tsit5()
 
 for ensalg in [ EnsembleSerial(), EnsembleThreads() ]
     @testset "Direct() with $ensalg" begin 
         tmax = 5. / Λ_gt
 
-        chem = Chemostat([ DECell(prob, divide_det) ])
+        chem = Chemostat([ DECell(prob, alg, divide_det) ])
         Chemostats.simulate!(chem, tmax, Chemostats.Direct(), ensalg) 
         snap = chem.snaps[end]
 
@@ -27,7 +28,7 @@ end
 tmax = 20.5 
 
 @testset "Strict($L)" for L in [ 1, 10 ]
-    chem = Chemostat([ DECell(prob, divide_det) for i in 1:L ])
+    chem = Chemostat([ DECell(prob, alg, divide_det) for i in 1:L ])
 
     @test_throws "Strict does not support parallelisation" Chemostats.simulate!(chem, tmax, Chemostats.Strict(L), EnsembleThreads())
 
@@ -44,7 +45,7 @@ end
 
 for ensalg in [ EnsembleSerial(), EnsembleThreads() ]
     @testset "Forward($L) with $ensalg" for L in [ 1, 10 ]
-        chem = Chemostat([ DECell(prob, divide_det) for i in 1:L ])
+        chem = Chemostat([ DECell(prob, alg, divide_det) for i in 1:L ])
         
         @test_throws DimensionMismatch Chemostats.simulate!(chem, tmax, Chemostats.Forward(L+1), ensalg)
 
--- test/growth.jl
+++ test/growth.jl
@@ -5,13 +5,14 @@ include("models/multitypemarkov.jl")
 
 prob = MultitypeMarkov([ 1., 2. ], [ 0.7 0.1; 0.3 0.9 ])
 Λ_gt = get_Λ_mtm(prob.p.model)
+alg = Tsit5()
 
 @testset "Strict(100)" begin
     tmax = 100 / Λ_gt 
     niter = 10
 
     ΛΛ = map(1:niter) do i
-        chem = Chemostat([ DECell(prob, divide_mtm) for i in 1:100 ])
+        chem = Chemostat([ DECell(prob, alg, divide_mtm) for i in 1:100 ])
         Chemostats.simulate!(chem, tmax, Chemostats.Strict(100)) 
         est_Λ(chem)
     end
@@ -25,7 +26,7 @@ for ensalg in [ EnsembleSerial(), EnsembleThreads() ]
         niter = 10
 
         ΛΛ = map(1:niter) do i
-            chem = Chemostat([ DECell(prob, divide_mtm) ])
+            chem = Chemostat([ DECell(prob, alg, divide_mtm) ])
             Chemostats.simulate!(chem, tmax, Chemostats.Lax(100, 0.5 / Λ_gt), ensalg; Nmax=1e4) 
             est_Λ(chem)
         end
